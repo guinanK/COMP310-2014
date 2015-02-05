287616510b4e7096d5ce779534cf8d584b048b01
Kieran Guinan <guinank1@wit.edu>
2015-02-05 08:48:33 -0500

    2/4 Classwork

--- workspace/Example/src/BinaryTreeNodeTest.java
+++ workspace/Example/src/BinaryTreeNodeTest.java
@@ -2,22 +2,24 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class BinaryTreeNodeTest {
-
+	
 	@Test
-	public void testAdd() {
-		BinaryTreeNode<String> tree =
-				new BinaryTreeNode<String>("dog");
-		tree.add("cat");
-		tree.add("fish");
-		tree.add("turtle");
-		tree.add("snake");
-		assertEquals("cat", tree.left.data);
-		assertEquals("fish", tree.right.data);
-		assertEquals("turtle", tree.right.right.data);
-		assertEquals("snake", tree.right.right.left.data);
-		assertTrue(tree.contains("snake"));
+	public void testAdd(){
+		BinaryTreeNode<String> root = new BinaryTreeNode<String>("dog");
+		root.add("cat");
+		root.add("fish");
+		root.add("turtle");
+		root.add("snake");
+		assertEquals("cat",root.left.data);
+		assertEquals("fish",root.right.data);
+		assertEquals("turtle",root.right.right.data);
+		assertEquals("snake", root.right.right.left.data);
+		assertTrue(root.contains("snake"));
+		assertTrue(root.contains("cat"));
+		assertTrue(root.contains("dog"));
+		assertTrue(root.contains("turtle"));
+		assertTrue(root.contains("fish"));
 	}
 
 }
